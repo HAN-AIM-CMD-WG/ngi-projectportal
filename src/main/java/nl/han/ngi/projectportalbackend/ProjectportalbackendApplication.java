a0043e3a636834f0f9ec3803c10052e55260f9d0
jveldmaat <93977495+jveldmaat@users.noreply.github.com>
2023-11-22 00:09:13 +0100

    initial application setup

---
+++ src/main/java/nl/han/ngi/projectportalbackend/ProjectportalbackendApplication.java
@@ -0,0 +1,13 @@
+package nl.han.ngi.projectportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectportalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectportalbackendApplication.class, args);
+	}
+
+}
